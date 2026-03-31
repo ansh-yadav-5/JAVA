cbd79c5c9df372a8d92a2e5e2058b88d495d2fb4
Ansh Yadav <anshyadav424@gmail.com>
2026-03-31 17:49:20 +0530

    31 march

--- F.java
+++ F.java
@@ -17,7 +17,7 @@
             )
             }
             public class MyInteger{
-                                                    public static void main(String[]args) {
+                                                    public void main(String[]args) {
                                                     }
                                                     value = MyInteger
                                                 }
